d5fc8725dbe2cfa8288955a521ee52117d43ba14
logic <logic.tanjueco12@gmail.com>
2023-10-11 18:06:47 +0800

    Added delete and edit feature for post

--- client/src/components/PostDetails.jsx
+++ client/src/components/PostDetails.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate, Link } from "react-router-dom";
 import { API_URL } from "../constants";
+import DeletePost from "./DeletePost";
 
 function PostDetails() {
   const [post, setPost] = useState(null);
-  const [loading,setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
   const { id } = useParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchCurrentPost = async () => {
@@ -14,7 +16,7 @@ function PostDetails() {
         if (response.ok) {
           const json = await response.json();
           setPost(json);
-          setLoading(false)
+          setLoading(false);
         } else {
           throw response;
         }
@@ -25,6 +27,21 @@ function PostDetails() {
     fetchCurrentPost();
   }, [id]);
 
+  const deletePost = async () => {
+    try {
+      const response = await fetch(`${API_URL}/${id}`, {
+        method: "delete",
+      });
+      if (response.ok) {
+        navigate("/");
+      } else {
+        throw reponse;
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   if (loading) {
     return <h1>Loading... </h1>;
   }
@@ -34,7 +51,11 @@ function PostDetails() {
       <p>Post details below</p>
       <h2>{post.title}</h2>
       <p>{post.body}</p>
+      <Link to={`/posts/${post.id}/edit`}>Edit</Link>
+      {" | "}
       <Link to="/">Back to Posts</Link>
+      {" | "}
+      <button onClick={deletePost}>Delete</button>
     </div>
   );
 }
--- client/src/components/PostsList.jsx
+++ client/src/components/PostsList.jsx
@@ -1,11 +1,27 @@
 import { useEffect, useState } from "react";
 import { API_URL } from "../constants";
 import { Link } from "react-router-dom";
+import DeletePost from "./DeletePost";
 
 function PostsList() {
   const [posts, setPosts] = useState([]);
   const [, setIsLoading] = useState([]);
   const [, setError] = useState(null);
+
+  const deletePost = async (id) => {
+    try {
+      const response = await fetch(`${API_URL}/${id}`, {
+        method: "delete",
+      });
+      if (response.ok) {
+        setPosts(posts.filter((post) => post.id !== id));
+      } else {
+        throw response;
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   // Fetch posts from the API
   useEffect(() => {
@@ -32,10 +48,13 @@ function PostsList() {
     <div>
       {posts.map((post, index) => (
         <div key={index} className="post-content">
-          <Link to={`posts/${post.id}`}>
-            {post.title}
-            </Link>
-          <p>{post.body}</p>
+          <h2>
+            <Link to={`posts/${post.id}`}>{post.title}</Link>
+          </h2>
+          <div className="post-links"></div>
+          <Link to={`posts/${post.id}/edit`}>Edit</Link>
+          <button onClick={() => deletePost(post.id)}>Delete</button>
+          <hr />
         </div>
       ))}
     </div>
